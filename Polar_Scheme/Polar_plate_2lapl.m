6bda92d2fc5654dab9a2216189d5cf85f8b5e041
Mateo92 <mateotonatiuh@ciencias.unam.mx>
2023-12-24 07:37:37 -0600

    Commented_Polar

--- Polar_Scheme/Polar_plate_2lapl.m
+++ Polar_Scheme/Polar_plate_2lapl.m
@@ -1,71 +1,98 @@
 function [out,SR,hr,ht,k]=...
-Polar_plate_2lapl(Di_ext,H_ext,E_ext,rho_ext,nu_ext,Di_int,H_int,E_int,rho_int,nu_int,E_scale_param,...
-kstability_constant,ctr,wid,u0,v0,sig0,k_sigmoid,logistic_fun,Nr,Nt,TF,rp_mat,ploting,anim,n_anim,limz,plot_fft,time_series)
+Polar_plate_2lapl(Di_ext,H_ext,E_ext,rho_ext,nu_ext,Di_int,H_int,E_int,...
+rho_int,nu_int,sigma_time,k_stability_constant,ctr,wid,u0,v0,sig0,...
+k_sigmoid,logistic_fun,Nr,Nt,TF,rp_mat,ploting,anim,n_anim,limz,plot_fft)
+%%%%%%%%%%%%%%%
+% Simulate a non-homogeneous plate using finite difference method
+%
+% Inputs:
+% Di_ext, Di_int: External and internal diameters of the plate.
+% H_ext, H_int: External and internal heights of the plate.
+% E_ext, E_int: Young's moduli of the external and internal materials.
+% rho_ext, rho_int: Densities of the external and internal materials.
+% nu_ext, nu_int: Poisson's ratios of the external and internal materials.
+% sigma_time: %Time scaling factor.
+% kstability_constant: Constant used for stability calculation.
+% ctr: Center coordinates for a velocity distribution calculation.
+% wid: Width parameter for the velocity distribution.
+% u0: Initial displacement.
+% v0: Initial velocity height.
+% sig0: Parameter for the velocity distribution function.
+% k_sigmoid: Parameter for a sigmoid function.
+% logistic_fun: Flag for selecting a logistic function.(1=true, 0= false)
+% Nr: Number of radial steps for discretization.
+% Nt: Number of angular steps for discretization.
+% TF: Final simulation time.
+% rp_mat: Matrix specifying readout positions.([0-1,0-2*pi])
+% ploting: Flag for enabling plot generation. (1=true, 0= false)
+% anim: Flag for enabling animation generation. (1=true, 0= false)
+% n_anim: Parameter for animation frequency.
+% limz: Parameter for plot z-axis limits.
+% plot_fft: Flag for enabling Fast Fourier Transform (FFT) plots. (1=true, 0= false)
+%
+% Outputs:
+% out: Simulation output for readout positions over time.
+% SR: Sample Rate calculated based on stability condition.
+% hr: Radial step size.
+% ht: Angular step size.
+% k: Time step for the simulation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-tic % For measuring the executing time of the program
-time_scale=strcat('10^{',num2str(log10(sqrt(E_scale_param))),'}s');
+tic % Start measuring execution time
 
-E_ext_scale=E_ext*E_scale_param; %Scale the Young modulus
-E_int_scale=E_int*E_scale_param;%Scale the Young modulus
+E_scale_param=sigma_time^2; % Scale parameter for the Young's modulus
 
-%%% Compute Stiffness parameters
+% Calculate time scale for display
+time_scale = strcat('10^{', num2str(log10(sqrt(E_scale_param))), '}s');
 
-D_ext=(E_ext_scale)*H_ext^3/(12*(1-nu_ext^2));
-K_ext=sqrt(D_ext/(rho_ext*H_ext*(Di_ext/2)^4));
+% Scale Young's modulus
+E_ext_scale = E_ext * E_scale_param;
+E_int_scale = E_int * E_scale_param;
 
-HT=H_int+H_ext;
+% Compute Stiffness parameters for exterior and interior
+D_ext = (E_ext_scale) * H_ext^3 / (12 * (1 - nu_ext^2));
+K_ext = sqrt(D_ext / (rho_ext * H_ext * (Di_ext / 2)^4));
+HT = H_int + H_ext;
+rho_l = HT / (H_ext / rho_ext + H_int / rho_int);
+E_l = HT / (H_ext / E_ext_scale + H_int / E_int_scale);
+nu_l = HT / (H_ext / nu_ext + H_int / nu_int);
+D_int = E_l * HT^3 / (12 * (1 - nu_l^2));
+K_int = sqrt(D_int / (rho_l * HT * (Di_ext / 2)^4));
 
-rho_l=HT/(H_ext/rho_ext+H_int/rho_int);
-E_l=HT/(H_ext/E_ext_scale+H_int/E_int_scale);
-nu_l=HT/(H_ext/nu_ext+H_int/nu_int);
+K_max = max([K_int, K_ext]);
+K_min = min([K_int, K_ext]);
+R_int = Di_int / Di_ext;
 
-D_int=E_l*HT^3/(12*(1-nu_l^2));
-K_int=sqrt(D_int/(rho_l*HT*(Di_ext/2)^4));
+% Radial coordinates step size and count
+hr = 1 / Nr;
+Nr = floor(1 / hr);
 
+% Angular coordinates step size and count
+ht = 2 * pi / Nt;
+Nt = round(2 * pi / ht);
 
-K_max=max([K_int,K_ext]);
+% Stability condition - calculate time step 'k'
+k = k_stability_constant * (hr^2 / (2 * K_max)) * (1 / (1 + 1 / (ht^2)));
 
-K_min=min([K_int,K_ext]);
+% Calculate Sample Rate
+SR =floor(1 / k);
 
-R_int=Di_int/Di_ext;
+% Number of Time Steps
+NF = floor(SR * TF);
 
-%%% Radial coordinates
+% Polar and Cartesian coordinates axes
+[R, T] = meshgrid([0:Nr] * hr * Di_ext / 2, [0:Nt - 1] * ht);
+[X, Y] = pol2cart(T, R);
 
-hr = 1/Nr;  % radial step size
-
-Nr=floor(1/hr);
-
-ht=2*pi/Nt; % angular step size
-
-Nt=round(2*pi/ht);
-
-%%% Stability condition 
-
- %plate free center
- k=kstability_constant*(hr^2/(2*K_max))*(1/(1+1/(ht^2)));
-%Sample Rate
-SR=1/k;
-
-%Number of Time Steps
-NF = floor(SR*TF);  
-
-%Polar coordinates axes
-[R, T] = meshgrid([0:Nr]*hr*Di_ext/2, [0:Nt-1]*ht);
-
-%Cartesian coordinates axes 
-
-[X,Y]=pol2cart(T,R);
-
-% Compute readout parameters index
-
-rp_mat_index=zeros(size(rp_mat));
-for i=1:size(rp_mat,1)
-    rp=rp_mat(i,:);
-    rp_r=rp(1);
-    rp_t=rp(2);
-    rpr_index=floor(rp_r/hr)+1;
-    rpt_index=floor(rp_t/ht)+1;
-    rp_mat_index(i,:)=[rpr_index rpt_index];
+% Compute readout parameters index for convenience
+rp_mat_index = zeros(size(rp_mat));
+for i = 1:size(rp_mat, 1)
+    rp = rp_mat(i, :);
+    rp_r = rp(1);
+    rp_t = rp(2);
+    rpr_index = floor(rp_r / hr) + 1;
+    rpt_index = floor(rp_t / ht) + 1;
+    rp_mat_index(i, :) = [rpr_index rpt_index];
 end
 
 % Compute rising cosine velocity distribution
@@ -101,14 +128,13 @@ end
 [x,y,K_mat_cart]=pol2cart(T,R,K_mat);
 
 
-%Fourth order polinomial Initial position
+% Fourth order polinomial Initial position
 qu=zeros(Nr+1,Nt);
 for i=1:Nr+1
     l=i-1;
     r=hr*l;
     for j=1:Nt
        qu(i,j)=(1-(r)^2)^2;
-        
     end
 end
 [x,y,qu_cart]=pol2cart(T,R,qu);
@@ -158,8 +184,6 @@ height=350;
 set(gcf,'position',[x0,y0,width,height])
 end
 
-%%
-
 % Initialize grid functions
 u_nm1 = u0*qu;
 u_n = u0*qu+k*v0*rc_pol;
@@ -183,11 +207,6 @@ end
 % Initialise test function v
 
 v=zeros(Nr+1,Nt);
-
-if time_series==1
-   figure(8)
-       tiledlayout(2,4)
-end
 %% Main loop
 for n=3:NF
 if mod(n,50000)==0
@@ -222,19 +241,12 @@ end
     
    end
 
-   
-
 %.........Center point v..............
 
 lapl_00=0;
   for m=0:Nt-1
       M=mod(m,Nt)+1;
  lapl_00=lapl_00+(4/((Nt)*hr^2))*(u_n(2,M)-u_n(1,M)); %forward first order aproximation
-%  lapl_00=lapl_00+(4/((Nt)*hr^2))*(-(11/6)*u_n(1,M)+3*u_n(2,M)-(3/2)*u_n(3,M)+(1/3)*u_n(4,M)); %forward third order aproximation
- 
-%  lapl_00=lapl_00+(4/((Nt)*hr^2))*(-(25/12)*u_n(1,M)+4*u_n(2,M)-(3)*u_n(3,M)+(4/3)*u_n(4,M)-(1/4)*u_n(5,M)); %forward fourth order aproximation
-
-%  lapl_00=lapl_00+(2/((Nt)*hr^2))*(u_n(2,M)-u_n(2,mod(m,floor(Nt/2))+1)); %centered first order aproximation
 
   end
 
@@ -266,12 +278,6 @@ lapl_00=0;
   for m=0:Nt-1
       M=mod(m,Nt)+1;
  lapl_00=lapl_00+(4/((Nt)*hr^2))*(v(2,M)-v(1,M));
-%  lapl_00=lapl_00+(4/((Nt)*hr^2))*(-(11/6)*v(1,M)+3*v(2,M)-(3/2)*v(3,M)+(1/3)*v(4,M)); %forward second order aproximation
-
-%  lapl_00=lapl_00+(4/((Nt)*hr^2))*(-(25/12)*v(1,M)+4*v(2,M)-(3)*v(3,M)+(4/3)*v(4,M)-(1/4)*v(5,M)); %forward fourth order aproximation
-
-
-%  lapl_00=lapl_00+(2/((Nt)*hr^2))*(v(2,M)-v(2,mod(m,floor(Nt/2))+1)); %centered first order aproximation
 
   end
 
@@ -288,7 +294,7 @@ lapl_00=0;
 
      if any(abs(u_n)>1e3)
          u_n(1,1)
-         'El esquema diverge'
+         'The scheme diverges'
          stop
      end
 
@@ -298,10 +304,11 @@ for i=1:size(rp_mat,1)
    out(n,i) = u_np1(rp_mat_index(i,1),rp_mat_index(i,2));
 end
 
-
+% Update grid funtion values
 
 u_nm1 = u_n; u_n = u_np1;
 
+% Animation
 
 if anim==1
    if mod(n,n_anim)==0
@@ -321,82 +328,55 @@ nexttile
    ylabel('$\theta$','Fontsize',17,'Interpreter','latex')
    zlabel('$u(r,\theta,t)$','Fontsize',17,'Interpreter','latex')
    zlim([-limz,limz])
-%    time_scale=strcat('10^{',num2str(log10(sqrt(E_scale_param))),'}s$');
    title(strcat('Time $t=',num2str(n*k,'%.4f'),'\times',time_scale,'$'),'Fontsize',20,'Interpreter','latex')
 nexttile
 hold on
    plot(hr*[0:Nr]*Di_ext/2,u_n(1:Nr+1,Nt),'o-','DisplayName','$u(r,\theta=0,t)$')
-   plot(hr*[0:Nr]*Di_ext/2,K_mat(1:Nr+1,1)/K_max*limz,'*-','DisplayName','$\kappa(r,\theta=0)$ (scaled)')
+   plot(hr*[0:Nr]*Di_ext/2,K_mat(1:Nr+1,1)/K_max*limz,'*-','DisplayName','$\kappa(r,\theta=0)$ (scale)')
    legend('Fontsize',17,'Interpreter','latex')
    ylim([-limz,limz])
    xlabel('$r[m]$','Fontsize',17,'Interpreter','latex')
    ylabel('$u(r,\theta=0,t)$','Fontsize',17,'Interpreter','latex')
    hold off
 x0=10;
-y0=75;
+y0=50;
 width=1400;
-height=300;
+height=350;
 set(gcf,'position',[x0,y0,width,height])
 
    drawnow limitrate
    end
 end
 
-if time_series==1
-%        a=[50,100,200,300,400,500,600,700];
-%         if ismember(n,a)
-[X,Y,u_n_cart]=pol2cart(T,R,u_n);
-for j=0:7
-    if n*k>=0.002+j*0.03 && n*k<0.002+j*0.03+k
-       %if (n*k>=0.002 && n*k<0.002+k) || (n*k>=0.004 && n*k<0.04+k) || (n*k>=0.006 && n*k<0.006+k) || (n*k>=0.003 && n*k<0.003+k) || (n*k>=0.004 && n*k<0.004+k) || (n*k>=0.005 && n*k<0.005+k) || (n*k>=0.006 && n*k<0.006+k) || (n*k>=0.007 && n*k<0.007+k)
-       nexttile
-       
-       mesh(X',Y',u_n_cart)
-    xlabel('$x[m]$','Fontsize',17,'Interpreter','latex')
-   ylabel('$y[m]$','Fontsize',17,'Interpreter','latex')
-   zlabel('$u(x,y,t)[m]$','Fontsize',17,'Interpreter','latex')
-   title(strcat('$t=',num2str(n*k,'%.3f'),'\times',time_scale,'$'),'Fontsize',20,'Interpreter','latex')
-   zlim([-limz,limz])
-
-    end
-end
-end
-
 end
 %%%%%% end main loop
-if time_series==1
-x0=0;
-        y0=50;
-        width=1500;
-        height=600;
-        set(gcf,'position',[x0,y0,width,height])
-end
+
 %% Plot waveforms
 if ploting==1
 out_sum=zeros(size(out(:,i)));
 t=[1:NF]*k;
 figure(3)
-% tiledlayout(2,1)
-% nexttile
+tiledlayout(2,1)
+nexttile
 hold on
 for i=1:size(rp_mat,1)
     leyenda=strcat('$(r,\theta)=$','(',num2str(rp_mat(i,1),'%.4f'),',',num2str(rp_mat(i,2),'%.4f'),')');
     plot(t,out(:,i),'DisplayName',leyenda)
     out_sum=out_sum+out(:,i);
 end
-lgd=legend('Fontsize',20,'Interpreter','latex');
+lgd=legend('Fontsize',17,'Interpreter','latex');
 title(lgd,'Read out position');
-ylabel('Amplitude $(\approx u(r,\theta))$ $[m]$','Fontsize',30,'Interpreter','latex')
-xlabel(strcat('Time $[\sigma]=[',time_scale,']$'),'Fontsize',30,'Interpreter','latex')
-% nexttile
-% plot(t,out_sum,'DisplayName','Sum of all readout points')
-% ylabel('Amplitude $[m]$','Fontsize',20,'Interpreter','latex')
-% xlabel(strcat('Time $[\sigma]=[',time_scale,']$'),'Fontsize',20,'Interpreter','latex')
-% legend('Fontsize',17,'Interpreter','latex')
+ylabel('Amplitude $[m]$','Fontsize',20,'Interpreter','latex')
+xlabel(strcat('Time $[\sigma]=[',time_scale,']$'),'Fontsize',20,'Interpreter','latex')
+nexttile
+plot(t,out_sum,'DisplayName','Sum of all readout points')
+ylabel('Amplitude $[m]$','Fontsize',20,'Interpreter','latex')
+xlabel(strcat('Time $[\sigma]=[',time_scale,']$'),'Fontsize',20,'Interpreter','latex')
+legend('Fontsize',17,'Interpreter','latex')
 x0=10;
 y0=500;
 width=1300;
-height=500;
+height=8500;
 set(gcf,'position',[x0,y0,width,height])
 end
 %% Plot FFT
@@ -406,7 +386,7 @@ tiledlayout(size(rp_mat,1),1)
 out_sum=zeros(size(out(:,i)));
 for i=1:size(rp_mat,1)
 out_sum=out_sum+out(:,i);
-Y = fft(out(:,i));       %Transformada de Fourier
+Y = fft(out(:,i));       % Fourier transform
 P2 = abs(Y/NF);      
 P1 = P2(1:NF/2+1);
 P1(2:end-1) = 2*P1(2:end-1); 
@@ -417,67 +397,49 @@ f = 1e-6/(sqrt(E_scale_param))*SR*(0:(NF/2))/NF;  % if [E]˜=E_scale_param
 [M,In]=max(P1);
 
 nexttile
-hold on
-read_out_point=strcat('$(r,\theta)=$','(',num2str(rp_mat(i,1),'%.4f'),',',num2str(rp_mat(i,2),'%.4f'),')');
-p=plot(f(1:floor(end/5)),P1(1:floor(end/5)));
-p.Annotation.LegendInformation.IconDisplayStyle = 'off';
-[pks,locs] =findpeaks(P1(1:floor(end/5)),f(1:floor(end/5)),'MinPeakDistance',0.5,...
-            'MinPeakHeight',M/70);
 
-[Mp,Inp]=max(pks);
-% Index numbers of elements to remove
-indicesToRemove = [1, Inp];
-% Create a logical index to keep elements not in the indicesToRemove list
-keepIndices = true(1, numel(pks));
-keepIndices(indicesToRemove) = false;
-pks_other=pks(keepIndices);
-locs_other=locs(keepIndices);
-% set(gca,'FontSize',80/size(rp_mat,1));
+findpeaks(P1(1:floor(end/5)),f(1:floor(end/5)),'MinPeakDistance',0.5,...
+            'MinPeakHeight',M/70)
+set(gca,'FontSize',80/size(rp_mat,1));
 xlabel('Frequency (MHz)','FontSize',88/size(rp_mat,1),'Interpreter','latex'); 
 ylabel('$|\hat{u}|$','FontSize',88/size(rp_mat,1),'Interpreter','latex');
 xlim([0,f(In)+50])
-freq_res=strcat('$f_{max}$=',num2str(f(In),'%.4f'),'MHz');
-f_1st=strcat('$f_{1^{st}}$=',num2str(locs(1),'%.4f'),'MHz');
-scatter(locs_other,pks_other,'^','DisplayName','Other modes')
-scatter(locs(1),pks(1),'o','DisplayName',f_1st)
-scatter(locs(Inp),pks(Inp),'*','DisplayName',freq_res)
-lgd=legend('Fontsize',17,'Interpreter','latex');
-title(lgd,read_out_point);
-hold off
-% tit=[freq_res newline read_out_point];
-% title(tit,'FontSize',80/size(rp_mat,1),'Interpreter','latex');
+read_out_point=strcat('$(r,\theta)=$','(',num2str(rp_mat(i,1),'%.4f'),',',num2str(rp_mat(i,2),'%.4f'),')');
+freq_res=strcat('Fourier Transform $f_r$=',num2str(f(In),'%.4f'),'MHz');
+tit=[freq_res newline read_out_point];
+title(tit,'FontSize',80/size(rp_mat,1),'Interpreter','latex');
 end
 x0=500;
 y0=500;
 width=800;
 height=1300;
 set(gcf,'position',[x0,y0,width,height])
-% Y = fft(out_sum);       %Transformada de Fourier
-% P2 = abs(Y/NF);      
-% P1 = P2(1:NF/2+1);
-% P1(2:end-1) = 2*P1(2:end-1); 
-% 
-% f = 1e-6/(sqrt(E_scale_param))*SR*(0:(NF/2))/NF;  % if [E]˜=E_scale_param
-% 
-% [M,In]=max(P1);
-% 
-% figure(5)
-% findpeaks(P1(1:floor(end/5)),f(1:floor(end/5)),'MinPeakDistance',0.5,...
-%             'MinPeakHeight',M/50,'Annotate','extents')
-% 
-% [pks,locs]=findpeaks(P1(1:floor(end/5)),f(1:floor(end/5)),'MinPeakDistance',2,...
-%             'MinPeakHeight',M/10,'Annotate','extents');
-% set(gca,'FontSize',20);
-% xlabel('Frequency (MHz)','FontSize',22,'Interpreter','latex'); 
-% ylabel('$|\hat{u}|$','FontSize',22,'Interpreter','latex');
-% xlim([0,max(locs)+5])
-% freq_res=strcat('$f_r$=',num2str(f(In),'%.4f'),'MHz');
-% tit=[freq_res newline 'Sum of all readout poins'];
-% title(tit,'FontSize',24,'Interpreter','latex');
-% x0=0;
-% y0=500;
-% width=800;
-% height=300;
-% set(gcf,'position',[x0,y0,width,height])
+Y = fft(out_sum);       %  Fourier transform
+P2 = abs(Y/NF);      
+P1 = P2(1:NF/2+1);
+P1(2:end-1) = 2*P1(2:end-1); 
+
+f = 1e-6/(sqrt(E_scale_param))*SR*(0:(NF/2))/NF;  % if [E]˜=E_scale_param
+
+[M,In]=max(P1);
+
+figure(5)
+findpeaks(P1(1:floor(end/5)),f(1:floor(end/5)),'MinPeakDistance',0.5,...
+            'MinPeakHeight',M/50,'Annotate','extents')
+
+[pks,locs]=findpeaks(P1(1:floor(end/5)),f(1:floor(end/5)),'MinPeakDistance',2,...
+            'MinPeakHeight',M/10,'Annotate','extents');
+set(gca,'FontSize',20);
+xlabel('Frequency (MHz)','FontSize',22,'Interpreter','latex'); 
+ylabel('$|\hat{u}|$','FontSize',22,'Interpreter','latex');
+xlim([0,max(locs)+5])
+freq_res=strcat('$f_r$=',num2str(f(In),'%.4f'),'MHz');
+tit=[freq_res newline 'Sum of all readout poins'];
+title(tit,'FontSize',24,'Interpreter','latex');
+x0=0;
+y0=500;
+width=800;
+height=300;
+set(gcf,'position',[x0,y0,width,height])
 end
-toc
+toc % Stop measuring execution time
--- Polar_Scheme/Run_Polar_Plate.m
+++ Polar_Scheme/Run_Polar_Plate.m
@@ -1,40 +1,44 @@
 % Some material properties %%%%%%%
+rho_au = 19.3 * 1e3;  % gold density kg/m3
+E_au = 79 * 1e9;  % gold Young modulus
+nu_au = 0.4;  % gold Poisson's ratio
 
-rho_au=19.3*1e3 ; %gold density kg/m3
-E_au=79*1e9 ; %gold Young modulus Pa=kg/(ms^2)
-nu_au=0.4 ; %gold Poisson's ratio
+rho_al = 2.7 * 1e3;  % Aluminium density kg/m3
+E_al = 70 * 1e9;  % Aluminium Young modulus 
+nu_al = 0.35;  % Aluminium Poisson's ratio
 
-rho_al=2.7*1e3 ;  %Aluminium density kg/m3
-E_al=70*1e9 ; %Aluminium Young modulus Pa=kg/(ms^2)
-nu_al=0.35 ; %Aluminium Poisson's ratio
+rho_cytop = 2.03 * 1e3;  % CYTOP density Kg/m3
+E_cytop = 7.9 * 1e9;  % CYTOP Young modulus Pa=kg/(ms^2)
+nu_cytop = 0.42;  % CYTOP Poisson's ratio
 
-rho_cytop=2.03*1e3 ; %CYTOP density Kg/m3
-E_cytop=7.9*1e9 ; %CYTOP Young modulus Pa=kg/(ms^2)
-nu_cytop=0.42 ; %CYTOP Poisson's ratio
+%%% External radius features %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Di_ext = 150 * 1e-6;
+H_ext = 15 * 1e-6;
+E_ext = E_cytop;
+rho_ext = rho_cytop;
+nu_ext = nu_cytop;
 
-%%% External radius features
-Di_ext=150*1e-6;   %External Diameter in [m]
-H_ext=15*1e-6;     %External thickness in [m]
-E_ext=E_cytop;     %External Young modulus
-rho_ext=rho_cytop; %External density
-nu_ext=nu_cytop;  %External Poisson's ratio
+%%% Internal radious features %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Di_int = Di_ext * 0.3;
+H_int = H_ext * 0.1;
+E_int = E_au;
+rho_int = rho_au;
+nu_int = nu_au;
 
-% %Homogeneous cytop plate first eigenfrequency
-D=E_cytop*H_ext^3/(12*(1-nu_cytop^2));
-kappa=sqrt(D/(rho_cytop*H_ext))/(Di_ext/2)^2
-f_res=kappa*10.22*1e-6/(2*pi)
-f_2=kappa*21.26*1e-6/(2*pi)
-f_3=kappa*34.88*1e-6/(2*pi)
-f_11=kappa*39.77*1e-6/(2*pi)
-f_40=kappa*51.04*1e-6/(2*pi)
-f_22=kappa*60.82*1e-6/(2*pi)
+%%% Homogeneous cytop plate first eigenfrequencies %%%%%%%%%%%%%%%%%%%%%%%%
+D = E_cytop * H_ext^3 / (12 * (1 - nu_cytop^2));
+kappa = sqrt(D / (rho_cytop * H_ext)) / (Di_ext / 2)^2;
+f_fund = kappa * 10.22 * 1e-6 / (2 * pi);
+f_11 = kappa * 21.26 * 1e-6 / (2 * pi);
+f_12 = kappa * 34.88 * 1e-6 / (2 * pi);
+f_20 = kappa * 39.77 * 1e-6 / (2 * pi);
+f_13 = kappa * 51.04 * 1e-6 / (2 * pi);
+f_21 = kappa * 60.82 * 1e-6 / (2 * pi);
 
+%%% Internal radious features %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% f_2/f_res
-%%
-%%% Internal radious features
-Di_int=Di_ext*0.5;  %Internal Diameter in [m]
-H_int=H_ext*1.54;      %Internal thickness in [m]
+Di_int=Di_ext*0.3;  %Internal Diameter in [m]
+H_int=H_ext*4;      %Internal thickness in [m]
 
 % Di_int=0;         %Homogeneous case
 % H_int=0;          %Homogeneous case
@@ -43,109 +47,87 @@ E_int=E_au;         %Internal Young modulus
 rho_int=rho_au;     %Internal density
 nu_int=nu_au;       %Internal Poisson's ratio
 
-%% Initial conditions parameters
+%%% Initial conditions parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%  Initial position
 
-% PE=0; %Initial potencial enegy [Joules]    
-% 
-% u0=Heigth_PE(PE,Di_int,nu_int,E_int,H_int,Di_ext,nu_ext,E_ext,H_ext); %initial position height [mm]
+ u0=0;       %initial position height [m]
+% u0=5e-8;
 
- u0=0.0;       %initial position height [m]
+%%% Initial velocity %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%% Initial velocity
-
-ctr = [0 0]*Di_ext/2; % center location in cartecian coordinates [x,y]
-wid = 0.25*Di_ext;  % width of excitation for initial velocity
-KE=1e-6;         %   Maximum initial kinetic energy [Joules]
+ctr = [0 0.2]*Di_ext/2; % center location in cartecian coordinates [x,y]
+wid = 0.2*Di_ext;  % width of excitation for initial velocity
+KE=7e-6;         %   Maximum initial kinetic energy [Joules]
 
 v0=init_vel_KE(KE,H_ext,rho_ext,Di_int,H_int,rho_int,wid); %maximum initial kinetic energy
-v0=130;
+
 % v0=5;         %maximum initial velocity [m/s]
 
-%%% Read out points
+%%% Read out points %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
- rp_mat = [[0 0];[0.7 0.5*pi];[0.9 1*pi];];   % position of readout([0-1,0-2*pi])
-%rp_mat = [[0 0];];   % position of readout([0-1,0-2*pi])
+rp_mat = [[0 0];[0.3 0.8*pi];[0.3 0.1*pi];];   % position of readout([0-1,0-2*pi])
+% rp_mat = [[0 0];];   % position of readout([0-1,0-2*pi])
 
-% [x,y]=pol2cart(0.5, 0.5*pi);
-% rp_mat_cart = [[0.3 0.2];[0.4 0.3];[0.45 0.25];...
-%               [0.5 0.5]; [0.6 0.15]; [0.7 0.8]]-0.5; % position of readout([x,y])
-% 
-% rp_mat=zeros(size(rp_mat_cart));
-% for i=1:size(rp_mat,1)
-%     rp=rp_mat_cart(i,:);
-%     rp_x=rp(1);
-%     rp_y=rp(2);
-%     [rp_th,rp_r]=cart2pol(rp_x,rp_y);
-%     while rp_th<0
-%     rp_th=rp_th+2*pi;
-%     end
-%     rp_mat(i,:)=[rp_r rp_th];
-% end
-% rp_mat(7,:)=[0.5, 0.5*pi]; % position of readout([0-1,0-2pi])
-% rp_mat
-%%
-
-%%% Stifnes parameter's parameters
+%%% Stifnes parameter's parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 logistic_fun=0;  %Decide is using logistic growth 1=true, 0= false
 k_sigmoid=55;   %Logistic growth parameter (only affects the simulation if logistic_fun==0)
 
-%%% Spacial mesh parameters
+%%% Spacial mesh parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Nr=30; %Number of radial points -1 := Nr-1 (because 0 is added later)
+Nr=40; %Number of radial points -1 := Nr-1 (because 0 is added later)
 
-Nt=30; %Number of angular points := Nt
+Nt=40; %Number of angular points := Nt
 
-%%% Time domain and loss parameter
+%%% Time domain and loss parameter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 sigma_time=1e-6;  %Time scaling factor
 
-% E_scale_param=1e-12;
-
 v0=v0*sigma_time; %Scaled Velocity
 
-E_scale_param=sigma_time^2; % Scale parameter for the Young's modulus
+k_stability_constant=1;
 
-kstability_constant=1;
-
-TF=10;             % simulation time [sqrt(E_scale_param)s]
-T60 = TF;       % 60 dB decay time [sqrt(E_scale_param)s]
+TF=2;             % simulation time [sigma_time*s]
+T60 = TF;       % 60 dB decay time [sigma_time*s]
 sig0 = 6*log(10)/T60;           % loss parameter
 
-% sig0=0;            % For free vibrations
+sig0=0;            % Undamped
 
-%%% Plot and animation
+%%% Plot and animation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 ploting=1;  %Plots are shown if ploting==1
 anim=1;     %Animation of the simulation is shown if anim==1
-n_anim=5000;   %Number of time steps between each frame in the animation
+n_anim=1500;   %Number of time steps between each frame in the animation
 plot_fft=1;  %Plot Fourier transform of the output signals  
-limz=1e-6;
-time_series=0;
-%%%Write metadata
+limz=1e-7;
+%%% Write metadata %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % T = table(PE,Di_ext,H_ext,E_ext,rho_ext,nu_ext,Di_int,H_int,E_int,rho_int,nu_int,...
 %     ctr,wid,u0,v0,sig0,k_sigmoid,logistic_fun,Nr,Nt,TF);
-% folder=strcat('CSV/Single_simulations/',datestr(datetime('now')),'/');
+% folder=strcat('Experiments/Single_experiment/',datestr(datetime('now')),'/');
 % mkdir(folder)
 % metadatafolder=strcat(folder,'/metadata/');
 % mkdir(metadatafolder)
 % writetable(T,strcat(metadatafolder,'metadata.csv'))
 % writematrix(rp_mat,strcat(metadatafolder,'rp_mat.csv'))
 
-% %%%%% Run simulation
+%%% Run simulation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
  [out_polar,SR,hr,ht,k_polar]=...
-      Polar_plate_2lapl(Di_ext,H_ext,E_ext,rho_ext,nu_ext,Di_int,H_int,E_int,rho_int,nu_int,E_scale_param,...
-kstability_constant,ctr,wid,u0,v0,sig0,k_sigmoid,logistic_fun,Nr,Nt,TF,rp_mat,ploting,anim,n_anim,limz,plot_fft,time_series);
+      Polar_plate_2lapl(Di_ext,H_ext,E_ext,rho_ext,nu_ext,Di_int,H_int,E_int,rho_int,nu_int,sigma_time,...
+k_stability_constant,ctr,wid,u0,v0,sig0,k_sigmoid,logistic_fun,Nr,Nt,TF,rp_mat,ploting,anim,n_anim,limz,plot_fft);
 
-%Write outputs
+%  [out_polar,SR,hr,ht,k_polar]=...
+%       Polar_plate_2lapl_noplot(Di_ext,H_ext,E_ext,rho_ext,nu_ext,Di_int,H_int,E_int,rho_int,nu_int,sigma_time,...
+% k_stability_constant,ctr,wid,u0,v0,sig0,k_sigmoid,logistic_fun,Nr,Nt,TF,rp_mat);
+
+
+%%% Write outputs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % writematrix(out,strcat(folder,'out.csv'));
 % writematrix(SR,strcat(folder,'SR.csv'));
 % writematrix(hr,strcat(folder,'hr.csv'));
 % writematrix(ht,strcat(folder,'ht.csv'));
-% writematrix(u0,strcat(folder,'u0.csv'));
+% writematrix(k_polar,strcat(folder,'k.csv'));
 
 
